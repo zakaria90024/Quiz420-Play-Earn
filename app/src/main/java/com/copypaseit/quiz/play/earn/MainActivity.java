aea1fb91ee0920e4fe8d57b0044897a21cb6e74a
MD ZAKARIA <zakaria112203@gmail.com>
2021-03-22 02:31:57 +0600

    Quiz420

---
+++ app/src/main/java/com/copypaseit/quiz/play/earn/MainActivity.java
@@ -0,0 +1,14 @@
+package com.copypaseit.quiz.play.earn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
